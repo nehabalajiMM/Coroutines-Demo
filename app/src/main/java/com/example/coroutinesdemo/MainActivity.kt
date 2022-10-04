389d327d0efa9393c99d98f6eae6a6d18a77a089
nehabalaji <neha.balaji@mutualmobile.com>
2022-10-04 00:21:10 -0700

    launch coroutine with lifecycleScope

--- app/src/main/java/com/example/coroutinesdemo/MainActivity.kt
+++ app/src/main/java/com/example/coroutinesdemo/MainActivity.kt
@@ -1,14 +1,12 @@
 package com.example.coroutinesdemo
 
-import android.nfc.Tag
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.TextView
+import android.widget.Button
+import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.*
-import kotlin.system.measureTimeMillis
-import kotlin.time.measureTime
-
 class MainActivity : AppCompatActivity() {
 
     val tag = "MainActivity"
@@ -17,55 +15,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val textView = findViewById<TextView>(R.id.textView)
+        val button = findViewById<Button>(R.id.buttonId)
 
-        Log.d(tag, "Before runblocking")
-//        val job = GlobalScope.launch(Dispatchers.Default) {
-//            Log.d(tag, "Starting long running calculation")
-//            withTimeout(2000L) {
-//                for (i in 30..42) {
-//                    if (isActive) {
-//                        Log.d(tag, "Result for i = $i: ${fib(i)}")
-//                    }
-//                }
-//            }
-//            Log.d(tag, "Ending long running calculation")
-
-        GlobalScope.launch(Dispatchers.IO) {
-            val time = measureTimeMillis {
-                val answer1 = async {
-                    networkCall1()
+        button.setOnClickListener {
+            lifecycleScope.launch {
+                while (true) {
+                    delay(1000L)
+                    Log.d(tag, "Still running...")
                 }
-                val answer2 = async {
-                    networkCall2()
-                }
-                Log.d(tag, "Answer1 is ${answer1.await()}")
-                Log.d(tag, "Answer2 is ${answer2.await()}")
             }
-            Log.d(tag, "Request took $time")
+
+            GlobalScope.launch {
+                delay(5000L)
+                Intent(this@MainActivity, SecondActivity::class.java).also {
+                    startActivity(it)
+                    finish()
+                }
+            }
         }
-
-//        runBlocking {
-//            delay(1000L)
-//            job.cancel()
-//            Log.d(tag, "Main thread is continuing..")
-//        }
-
     }
 
-    suspend fun networkCall1(): String {
-        delay(3000L)
-        return "Answer 1"
-    }
-
-    suspend fun networkCall2(): String {
-        delay(3000L)
-        return "Answer 2"
-    }
-
-    fun fib(n: Int): Long {
-        return if (n==0) 0
-        else if (n==1) 1
-        else fib(n-1)+fib(n-2)
-    }
 }
