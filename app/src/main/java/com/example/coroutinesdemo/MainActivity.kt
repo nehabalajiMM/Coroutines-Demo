1d6b45f264cdf3ba2b15f5962c60fbae57a859d8
nehabalaji <neha.balaji@mutualmobile.com>
2022-10-03 22:07:22 -0700

    switch coroutine context

--- app/src/main/java/com/example/coroutinesdemo/MainActivity.kt
+++ app/src/main/java/com/example/coroutinesdemo/MainActivity.kt
@@ -4,31 +4,28 @@ import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import android.widget.TextView
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
 
     val tag = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        GlobalScope.launch {
+        val textView = findViewById<TextView>(R.id.textView)
+
+        GlobalScope.launch(Dispatchers.IO) {
             val networkCallAnswer = doNetworkCall()
-            val networkCallAnswer2 = doNetworkCall2()
-            Log.d(tag, networkCallAnswer)
-            Log.d(tag, networkCallAnswer2)
+            withContext(Dispatchers.Main) {
+                textView.text = networkCallAnswer
+            }
         }
     }
 
     suspend fun doNetworkCall(): String {
-        delay(3000L)
-        return "This is the answer"
-    }
-
-    suspend fun doNetworkCall2(): String {
         delay(3000L)
         return "This is the answer"
     }
